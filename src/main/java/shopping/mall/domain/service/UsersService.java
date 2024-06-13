a61556a15cf3c4ba68e7bec8cf431ed213540845
kimsanhaa <ttksgk@gmail.com>
2024-06-13 13:12:09 +0900

    Feat: user signUp 기능 추가중

--- src/main/java/shopping/mall/domain/service/UsersService.java
+++ src/main/java/shopping/mall/domain/service/UsersService.java
@@ -1,5 +1,24 @@
 package shopping.mall.domain.service;
 
-public class UsersService {
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import shopping.mall.domain.entities.Users;
+import shopping.mall.domain.repositories.UserMapper;
+import shopping.mall.domain.repositories.UsersRepository;
+import shopping.mall.ui.api.request.SignUpUsersRequest;
 
+@AllArgsConstructor
+@Service
+public class UsersService {
+    private final UsersRepository usersRepository;
+    private final UserMapper userMapper;
+
+    public void signUp(SignUpUsersRequest request) {
+        Users user = new Users().create(request);
+        if(userMapper.findUserIdByEmail(request.getEmail()) != null){
+            throw new IllegalArgumentException("중복된 이메일이 있습니다.");
+        }
+        usersRepository.save(user);
+
+    }
 }
--- src/main/java/shopping/mall/ui/api/UserController.java
+++ src/main/java/shopping/mall/ui/api/UserController.java
@@ -1,9 +1,22 @@
 package shopping.mall.ui.api;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import shopping.mall.domain.service.UsersService;
+import shopping.mall.ui.api.request.SignUpUsersRequest;
 
+@AllArgsConstructor
 @RestController
 public class UserController {
+    private final UsersService usersService;
 
+    @PostMapping("/users")
+    public void userSignUp(@RequestBody SignUpUsersRequest request){
+        usersService.signUp(request);
+
+    }
 }
