3f53ed08f9b7950129fa4a597e81a27d87d11e16
kimsanhaa <ttksgk@gmail.com>
2024-06-16 22:59:28 +0900

    Fix: jwt vo 삭제 / jwt vo test 삭제

--- src/test/java/shopping/mall/domain/repositories/UserRepositoryTest.java
+++ src/test/java/shopping/mall/domain/repositories/UserRepositoryTest.java
@@ -1,34 +1,34 @@
-package shopping.mall.domain.repositories;
-
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import shopping.mall.domain.entities.Users;
-
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Disabled
-class UserRepositoryTest {
-    @Autowired
-    private UserRepository userRepository;
-
-    @Test
-    public void sut는_user를_저장한다(){
-        //Arrange
-        UserRepository sut = userRepository;
-        String email = "jjim1234@jjim.com";
-        String password = "jjim1234";
-        Users user = new Users(email, password);
-
-        //Act
-        Users users = sut.save(user);
-
-        //Assert
-        Assertions.assertThat(users).isNotNull();
-        Assertions.assertThat(users.getEmail()).isEqualTo(email);
-        Assertions.assertThat(users.getPassword()).isEqualTo(password);
-    }
-}
+//package shopping.mall.domain.repositories;
+//
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//import shopping.mall.domain.entities.Users;
+//
+//@DataJpaTest
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@Disabled
+//class UserRepositoryTest {
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Test
+//    public void sut는_user를_저장한다(){
+//        //Arrange
+//        UserRepository sut = userRepository;
+//        String email = "jjim1234@jjim.com";
+//        String password = "jjim1234";
+//        Users user = new Users(email, password);
+//
+//        //Act
+//        Users users = sut.save(user);
+//
+//        //Assert
+//        Assertions.assertThat(users).isNotNull();
+//        Assertions.assertThat(users.getEmail()).isEqualTo(email);
+//        Assertions.assertThat(users.getPassword()).isEqualTo(password);
+//    }
+//}
